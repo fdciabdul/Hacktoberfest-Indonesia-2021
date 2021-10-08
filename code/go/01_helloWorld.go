5123cd491e7725c5da5e823296cfbc38d60ba40e
Hasobi <13960580+hasobi@users.noreply.github.com>
2021-10-08 17:45:12 +0700

    edit helloworld dan isi beberapa penjelasan dasar mengenai fundamaental go

--- code/go/01_helloWorld.go
+++ code/go/01_helloWorld.go
@@ -1,7 +1,26 @@
 package main
+/*
+Deklarasi package main merupakan penanda untuk go compiler
+agar package di-compile sebagai executable program dan bukan sebagai library.
+*/
 
 import "fmt"
+/*
+package fmt atau format package merupakan package dari go untuk memodifikasi output berupa string, int,
+atau format lainnya.
+*/
+/*
+Untuk memanggil package lebih dari satu, misal fmt dan strings, kita bisa menulis dengan
+
+import (
+	"fmt"
+	"strings"
+)
+*/
 
 func main()  {
 	fmt.Println("Hello World!")
+	/*
+	baris kode di atas merupakan baris dasar untuk menampilkan Hello World! ke konsol.
+	*/
 }
